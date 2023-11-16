3ddff6106802826cb64e1480b0596167e1dbb8e7
Joncross202 <cro21035@byui.edu>
2023-11-15 21:46:19 -0500

    made music section work

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -48,32 +48,55 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // Music section 
 
+
+
 document.addEventListener("DOMContentLoaded", function () {
     var audio = document.getElementById("myAudio");
     var audioSource = document.getElementById("audioSource");
     var hoverElement = document.getElementById("hoverElement");
 
-    var songs = [
-        { name: "Heart and Soul", src: "music/Heart and Soul.mp3" },
-        { name: "Random song March 9th, 2023", src: "music/Random Freestyle loop Mar9.mp3" },
+    var originalSongs = [
+        { name: "Heart and Soul", src: "/music/Heart and Soul.mp3" },
+        { name: "Random Freestyle Mar 9th, 2023", src: "/music/Random Freestyle loop Mar9.mp3" },
+        { name: "Random Freestyle Mar 10th, 2023", src: "music/Freestyle loop mar10.mp3" },
+        { name: "Random Freestyle Mar 11th, 2023", src: "/music/freestyle loop mar 11.mp3" },
+        { name: "Random Freestyle Mar 11th, 2023", src: "/music/Freestyle loop mar11 part 2.mp3" },
+        { name: "Random Freestyle Mar 12th, 2023", src: "/music/freestyle loop mar 12.mp3" },
+        { name: "Random Freestyle Apr 5th, 2023", src: "/music/freestyle april 5th.mp3" },
+        { name: "Poor Wayfaring Man of Grief", src: "/music/Poor Wayfaring Man of Grief.mp3" },
+
         // Add more songs as needed
     ];
 
-    hoverElement.addEventListener("mouseover", function () {
-        var randomIndex = Math.floor(Math.random() * songs.length);
-        var randomSong = songs[randomIndex];
+    // Create a copy of the original array
+    var songs = originalSongs.slice();
 
-        audioSource.src = randomSong.src;
-        audio.load();
-        audio.play();
+    function playRandomSong() {
+        if (songs.length > 0) {
+            var randomIndex = Math.floor(Math.random() * songs.length);
+            var randomSong = songs[randomIndex];
 
-        // Display the name of the song
-        console.log("Now playing: " + randomSong.name);
-    });
+            audioSource.src = randomSong.src;
+            audio.load();
+            audio.play();
+
+            // Display the name of the song
+            console.log("Now playing: " + randomSong.name);
+
+            // Remove the played song from the array
+            songs.splice(randomIndex, 1);
+        } else {
+            // If all songs have been played, reset the array
+            songs = originalSongs.slice();
+        }
+    }
+
+    hoverElement.addEventListener("mouseover", playRandomSong);
 
     hoverElement.addEventListener("mouseout", function () {
         audio.pause();
         audio.currentTime = 0;
     });
 });
+
 
